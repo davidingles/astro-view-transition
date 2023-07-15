e3989061abe93e52a08a7fdac93d553cb04d8bec
davidingles <davidingleslopez@gmail.com>
2023-07-15 20:43:33 +0200

    view transition con paginas dinamicas

---
+++ src/data/book.js
@@ -0,0 +1,33 @@
+export const books = [
+    {
+        id:'clean-code',
+        title: 'Clean Code: Manual de desarrollo',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg",
+        opinion: "Ut amet cillum deserunt fugiat deserunt ex enim laboris ea nulla id elit sint eu. Ipsum nostrud adipisicing eu fugiat dolore ad aliqua irure pariatur enim fugiat. Magna eu veniam excepteur incididunt ad et ipsum esse. Mollit excepteur est quis mollit minim id nulla do ullamco. Ut ipsum fugiat cillum labore occaecat duis est duis exercitation aliquip. Qui exercitation id eiusmod culpa anim cupidatat do enim in.",
+        author: 'Arturo Perez Reverte',
+        
+    },
+    {
+        id:'mierda-code',
+        title: 'Mierda Code: Manual de desarrollo',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+        opinion: "un libro para olvidar",
+        author: 'Benito Perez Galdós',
+        
+    },
+    {
+        id:'java-code',
+        title: 'java Code: Manual de desarrollo',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1391032527i/43713.jpg",
+        opinion: "Una obra maestra",
+        author: 'David Inglés',
+    },
+    {
+        id:'python-code',
+        title: 'python Code: Manual de desarrollo',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1408309444i/58128.jpg",
+        opinion: "Guarda tu dinero",
+        author: 'Mejor no hubieras nacido',
+    }
+    
+]
